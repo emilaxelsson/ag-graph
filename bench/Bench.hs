ae0dfd8e5d55550da8c00da79ecd3f9e6a95633b
Patrick Bahr <patrick.bahr@gmail.com>
2014-09-10 16:21:23 +0200

    benchmarks for the paper

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -143,60 +143,60 @@ reduceGST = fromEnum . Dag.runAGDagST max value depth (const 0)
 bench' str f arg = rnf arg `seq` bench str (nf f arg)
 
 reduce_expTree n = bgroup "expTree"
-    [bench' (show n) reduce $ expTree n | n <- [12..n]]
+    [bench' (show n) reduce $ expTree n | n <- [startN..n]]
   -- Grows exponentially
 
 reduce_expSimple n = bgroup "expSimple"
-    [bench' (show n) reduceGS $ expSimple n | n <- [12..n]]
+    [bench' (show n) reduceGS $ expSimple n | n <- [startN..n]]
 
 reduce_expSimpleST n = bgroup "expSimpleST"
-    [bench' (show n) reduceGSST $ expSimple n | n <- [12..n]]
+    [bench' (show n) reduceGSST $ expSimple n | n <- [startN..n]]
 
 reduce_expFree n = bgroup "expFree"
-    [bench' (show n) reduceGF $ expFree n | n <- [12..n]]
+    [bench' (show n) reduceGF $ expFree n | n <- [startN..n]]
 
 reduce_expDag n = bgroup "expDag"
-    [bench' (show n) reduceG $ expDag n | n <- [12..n]]
+    [bench' (show n) reduceG $ expDag n | n <- [startN..n]]
 
 
 reduce_expFreeST n = bgroup "expFreeST"
-    [bench' (show n) reduceGFST $ expFree n | n <- [12..n]]
+    [bench' (show n) reduceGFST $ expFree n | n <- [startN..n]]
 
 reduce_expDagST n = bgroup "expDagST"
-    [bench' (show n) reduceGST $ expDag n | n <- [12..n]]
+    [bench' (show n) reduceGST $ expDag n | n <- [startN..n]]
 
 
 reduce_linearSimple n = bgroup "linearSimple"
-    [bench' (show n) reduceGS $ linearSimple n | n <- [12..n]]
+    [bench' (show n) reduceGS $ linearSimple n | n <- [startN..n]]
   -- Grows linearly
 
 reduce_linearSimpleST n = bgroup "linearSimpleST"
-    [bench' (show n) reduceGSST $ linearSimple n | n <- [12..n]]
+    [bench' (show n) reduceGSST $ linearSimple n | n <- [startN..n]]
   -- Grows linearly
 
 
 reduce_linearFree n = bgroup "linearFree"
-    [bench' (show n) reduceGF $ linearFree n | n <- [12..n]]
+    [bench' (show n) reduceGF $ linearFree n | n <- [startN..n]]
   -- Grows linearly
 
 reduce_linearFreeST n = bgroup "linearFreeST"
-    [bench' (show n) reduceGFST $ linearFree n | n <- [12..n]]
+    [bench' (show n) reduceGFST $ linearFree n | n <- [startN..n]]
   -- Grows linearly
 
 reduce_linearDag n = bgroup "linearDag"
-    [bench' (show n) reduceG $ linearDag n | n <- [12..n]]
+    [bench' (show n) reduceG $ linearDag n | n <- [startN..n]]
   -- Grows linearly
 
 reduce_linearDagST n = bgroup "linearDagST"
-    [bench' (show n) reduceGST $ linearDag n | n <- [12..n]]
+    [bench' (show n) reduceGST $ linearDag n | n <- [startN..n]]
   -- Grows linearly
 
 
-reduce_linearDagBigS n = bgroup "linearDagBigS"
+reduce_linearSimpleBig n = bgroup "linearSimpleBig"
     [bench' (show n) reduceGS $ linearSimple n | n <- [100,200..n]]
   -- Grows linearly even for sizes that are out of reach for `reduce`
 
-reduce_linearDagBigSST n = bgroup "linearDagBigSST"
+reduce_linearSimpleBigST n = bgroup "linearSimpleBigST"
     [bench' (show n) reduceGSST $ linearSimple n | n <- [100,200..n]]
   -- Grows linearly even for sizes that are out of reach for `reduce`
 
@@ -254,36 +254,36 @@ repminGST = snd . Dag.runRewriteDagST const minS minI rep' init
 -- that simply forcing a bit result takes some time.
 
 repmin_expTree n = bgroup "expTree"
-    [bench' (show n) repmin $ expTree n | n <- [12..n]]
+    [bench' (show n) repmin $ expTree n | n <- [startN..n]]
   -- Grows exponentially
 
 
 repmin_expFreeST n = bgroup "expFreeST"
-    [bench' (show n) repminFreeST $ expFree n | n <- [12..n]]
+    [bench' (show n) repminFreeST $ expFree n | n <- [startN..n]]
 
 repmin_expDagST n = bgroup "expDagST"
-    [bench' (show n) repminGST $ expDag n | n <- [12..n]]
+    [bench' (show n) repminGST $ expDag n | n <- [startN..n]]
 
 repmin_expSimple n = bgroup "expSimple"
-    [bench' (show n) repminSimple $ expSimple n | n <- [12..n]]
+    [bench' (show n) repminSimple $ expSimple n | n <- [startN..n]]
 
 repmin_expSimpleST n = bgroup "expSimpleST"
-    [bench' (show n) repminSimpleST $ expSimple n | n <- [12..n]]
+    [bench' (show n) repminSimpleST $ expSimple n | n <- [startN..n]]
 
 
 repmin_linearSimple n = bgroup "linearSimple"
-    [bench' (show n) repminSimple $ linearSimple n | n <- [12..n]]
+    [bench' (show n) repminSimple $ linearSimple n | n <- [startN..n]]
 
 
 repmin_linearSimpleST n = bgroup "linearSimpleST"
-    [bench' (show n) repminSimpleST $ linearSimple n | n <- [12..n]]
+    [bench' (show n) repminSimpleST $ linearSimple n | n <- [startN..n]]
 
 
 repmin_linearFreeST n = bgroup "linearFreeST"
-    [bench' (show n) repminFreeST $ linearFree n | n <- [12..n]]
+    [bench' (show n) repminFreeST $ linearFree n | n <- [startN..n]]
 
 repmin_linearDagST n = bgroup "linearDagST"
-    [bench' (show n) repminGST $ linearDag n | n <- [12..n]]
+    [bench' (show n) repminGST $ linearDag n | n <- [startN..n]]
 
 
 repmin_linearSimpleBig n = bgroup "linearSimpleBig"
@@ -300,44 +300,71 @@ repmin_linearFreeBigST n = bgroup "linearFreeBigST"
 repmin_linearDagBigST n = bgroup "linearDagBigST"
     [bench' (show n) repminGST $ linearDag n | n <- [100,200..n]]
 
-
+startN = 4
 
 
 main = do
-    defaultMainWith (conf "reduce_expTree")            [reduce_expTree           16]
-    defaultMainWith (conf "reduce_expSimple")          [reduce_expSimple         16]
-    defaultMainWith (conf "reduce_expSimpleST")        [reduce_expSimpleST       16]
-    defaultMainWith (conf "reduce_expFree")            [reduce_expFree           16]
-    defaultMainWith (conf "reduce_expFreeST")          [reduce_expFreeST         16]
-    defaultMainWith (conf "reduce_expDag")             [reduce_expDag            16]
-    defaultMainWith (conf "reduce_expDagST")           [reduce_expDagST          16]
 
-    defaultMainWith (conf "reduce_linearSimple")       [reduce_linearSimple      16]
-    defaultMainWith (conf "reduce_linearSimpleST")     [reduce_linearSimpleST    16]
-    defaultMainWith (conf "reduce_linearFree")         [reduce_linearFree        16]
-    defaultMainWith (conf "reduce_linearFreeST")       [reduce_linearFreeST      16]
-    defaultMainWith (conf "reduce_linearDag")          [reduce_linearDag         16]
+    -- all benchmarks --
+ 
+    -- defaultMainWith (conf "reduce_expTree")            [reduce_expTree           16]
+    -- defaultMainWith (conf "reduce_expSimple")          [reduce_expSimple         16]
+    -- defaultMainWith (conf "reduce_expSimpleST")        [reduce_expSimpleST       16]
+    -- defaultMainWith (conf "reduce_expFree")            [reduce_expFree           16]
+    -- defaultMainWith (conf "reduce_expFreeST")          [reduce_expFreeST         16]
+    -- defaultMainWith (conf "reduce_expDag")             [reduce_expDag            16]
+    -- defaultMainWith (conf "reduce_expDagST")           [reduce_expDagST          16]
+
+    -- defaultMainWith (conf "reduce_linearSimple")       [reduce_linearSimple      16]
+    -- defaultMainWith (conf "reduce_linearSimpleST")     [reduce_linearSimpleST    16]
+    -- defaultMainWith (conf "reduce_linearFree")         [reduce_linearFree        16]
+    -- defaultMainWith (conf "reduce_linearFreeST")       [reduce_linearFreeST      16]
+    -- defaultMainWith (conf "reduce_linearDag")          [reduce_linearDag         16]
+    -- defaultMainWith (conf "reduce_linearDagST")        [reduce_linearDagST       16]
+
+    -- defaultMainWith (conf "reduce_big_linearSimple")   [reduce_linearSimpleBig     1000]
+    -- defaultMainWith (conf "reduce_big_linearSimpleST") [reduce_linearSimpleBigST   1000]
+    -- defaultMainWith (conf "reduce_big_linearFree")     [reduce_linearFreeBig     1000]
+    -- defaultMainWith (conf "reduce_big_linearFreeST")   [reduce_linearFreeBigST   1000]
+    -- defaultMainWith (conf "reduce_big_linearDag")      [reduce_linearDagBig      1000]
+    -- defaultMainWith (conf "reduce_big_linearDagST")    [reduce_linearDagBigST    1000]
+
+    -- defaultMainWith (conf "repmin_expTree")            [repmin_expTree           16]
+    -- defaultMainWith (conf "repmin_expSimple")          [repmin_expSimple         12]  -- OBS only up to 12
+    -- defaultMainWith (conf "repmin_expSimpleST")        [repmin_expSimpleST       12]
+    -- defaultMainWith (conf "repmin_expFreeST")          [repmin_expFreeST         16]
+    -- defaultMainWith (conf "repmin_expDagST")           [repmin_expDagST          16]
+
+    -- defaultMainWith (conf "repmin_linearSimple")       [repmin_linearSimple      16]
+    -- defaultMainWith (conf "repmin_linearSimpleST")     [repmin_linearSimpleST    16]
+    -- defaultMainWith (conf "repmin_linearFreeST")       [repmin_linearFreeST      16]
+    -- defaultMainWith (conf "repmin_linearDagST")        [repmin_linearDagST       16]
+
+    -- defaultMainWith (conf "repmin_big_linearSimple")   [repmin_linearSimpleBig   1000]
+    -- defaultMainWith (conf "repmin_big_linearSimpleST") [repmin_linearSimpleBigST 1000]
+    -- defaultMainWith (conf "repmin_big_linearFreeST")   [repmin_linearFreeBigST   1000]
+    -- defaultMainWith (conf "repmin_big_linearDagST")    [repmin_linearDagBigST    1000]
+
+
+    -- benchmarks from the paper --
+
+    defaultMainWith (conf "reduce_expTree")            [reduce_expTree           16]
+    defaultMainWith (conf "reduce_expDagST")           [reduce_expDagST          16]
     defaultMainWith (conf "reduce_linearDagST")        [reduce_linearDagST       16]
 
-    defaultMainWith (conf "reduce_big_linearSimple")   [reduce_linearDagBigS     1000]
-    defaultMainWith (conf "reduce_big_linearSimpleST") [reduce_linearDagBigSST   1000]
-    defaultMainWith (conf "reduce_big_linearFree")     [reduce_linearFreeBig     1000]
-    defaultMainWith (conf "reduce_big_linearFreeST")   [reduce_linearFreeBigST   1000]
-    defaultMainWith (conf "reduce_big_linearDag")      [reduce_linearDagBig      1000]
+    defaultMainWith (conf "reduce_expSimple")          [reduce_expSimple         16]
+    defaultMainWith (conf "reduce_linearSimple")       [reduce_linearSimple      16]
+
+    defaultMainWith (conf "reduce_big_linearSimpleST") [reduce_linearSimpleBigST 1000]
     defaultMainWith (conf "reduce_big_linearDagST")    [reduce_linearDagBigST    1000]
 
+
     defaultMainWith (conf "repmin_expTree")            [repmin_expTree           16]
-    defaultMainWith (conf "repmin_expSimple")          [repmin_expSimple         12]  -- OBS only up to 12
-    defaultMainWith (conf "repmin_expSimpleST")        [repmin_expSimpleST       12]
-    defaultMainWith (conf "repmin_expFreeST")          [repmin_expFreeST         16]
+    defaultMainWith (conf "repmin_expSimpleST")        [repmin_expSimpleST       16]
     defaultMainWith (conf "repmin_expDagST")           [repmin_expDagST          16]
 
-    defaultMainWith (conf "repmin_linearSimple")       [repmin_linearSimple      16]
     defaultMainWith (conf "repmin_linearSimpleST")     [repmin_linearSimpleST    16]
-    defaultMainWith (conf "repmin_linearFreeST")       [repmin_linearFreeST      16]
     defaultMainWith (conf "repmin_linearDagST")        [repmin_linearDagST       16]
 
-    defaultMainWith (conf "repmin_big_linearSimple")   [repmin_linearSimpleBig   1000]
     defaultMainWith (conf "repmin_big_linearSimpleST") [repmin_linearSimpleBigST 1000]
-    defaultMainWith (conf "repmin_big_linearFreeST")   [repmin_linearFreeBigST   1000]
     defaultMainWith (conf "repmin_big_linearDagST")    [repmin_linearDagBigST    1000]
